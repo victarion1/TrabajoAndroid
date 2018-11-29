f2d3dfdf1cc52da497e014f2e2d276e2c98caabd
Amatheratzu <marco.valdes.valdivia@gmail.com>
2018-11-28 23:21:03 -0300

    Login Facebook
    Configuracion de Login de Facebook

--- AppSession2/app/src/main/java/com/example/marco/appsession2/Main2Activity.java
+++ AppSession2/app/src/main/java/com/example/marco/appsession2/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.marco.appsession2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.AccessToken;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        if (AccessToken.getCurrentAccessToken()==null){
+            goLoginScreen();
+        }
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- AppSession2/app/src/main/java/com/example/marco/appsession2/MainActivity.java
+++ AppSession2/app/src/main/java/com/example/marco/appsession2/MainActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edt_usuario, edt_contraseña;
     private Button btn1;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         edt_usuario = (EditText)findViewById(R.id.edt_usuario);
         edt_contraseña = (EditText)findViewById(R.id.edt_contraseña);
@@ -32,9 +42,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                goMainScreen();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(),R.string.cancel_login,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),R.string.error_login,Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this,Main2Activity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+}
+
 
 
 }
