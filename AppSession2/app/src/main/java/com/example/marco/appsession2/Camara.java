f8113ca3ac908943b82d5e733170f6b97c1e47d2
victarion1 <crls.hdlg@gmail.com>
2018-12-03 20:03:40 -0300

    q

--- AppSession2/app/src/main/java/com/example/marco/appsession2/Camara.java
+++ AppSession2/app/src/main/java/com/example/marco/appsession2/Camara.java
@@ -1,5 +1,13 @@
-package com.example.carlos.camara;
+package com.example.marco.appsession2;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.hardware.camera2.CameraManager;
@@ -13,34 +21,14 @@ import android.view.View.OnClickListener;
 import android.content.Intent;
 import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity  {
-
+public class Camara {
     Button btn;
     ImageView imagen;
     Intent i;
-    final static int cons =0;
+
     Bitmap bmp;
 
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.btnFoto);
-        imagen = (ImageView) findViewById(R.id.imageView);
-
-        btn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Prueba", Toast.LENGTH_SHORT).show();
-
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i,cons);
-            }
-        });
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -51,4 +39,6 @@ public class MainActivity extends AppCompatActivity  {
             imagen.setImageBitmap(bmp);
         }
     }
+}
+
 }
